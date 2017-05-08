e9fa5c018e38494d094efe0ec47e8a944ebcca84
Winston Tokuhisa <tokuhisawinston@outlook.com>
2017-05-08 19:11:16 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
 var name = "Joe"
 var height = "74"
 
+var name = "name"
+var name = "Janet"
+
+var height = "67"
+var height = 74
+
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+// console.log(height)
+console.log(height+1)
+// console.log(parseInt(height) + 1 )
