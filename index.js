d4b5c556f379b4a17a48d2396004c4d6f34deb4b
khadija Bouallou <kb22704@hotmail.com>
2017-05-23 12:25:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// write your code below!
-
-var name = "Joe"
-var height = "74"
+// write your code below
+var name = "Janet"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height + 1)
