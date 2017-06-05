dd0c2382331450e31f9f4c190788360ef65b0c94
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-06-05 22:20:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
-
-var name = "David"
-var height = 74
+var name = "Joe"
+var height = "74"
+var message = `${name} is ${height} inches tall`
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height + 1)
+console.log(message)
