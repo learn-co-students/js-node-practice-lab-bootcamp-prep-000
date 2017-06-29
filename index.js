f8ad2407efe910bf73d0f7f89098b8075513e7a8
Jerry Noel <noel.jerry94@gmail.com>
2017-06-29 15:13:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Jerry Noel"
-var height = "74"
+var height = 15
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 - 59 )
+console.log(height + 1  )
