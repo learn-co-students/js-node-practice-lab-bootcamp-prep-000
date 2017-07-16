3520676309770e2b7d629855da2fbe91638c5999
Chi Nguyen <lncnguyen@gmail.com>
2017-07-16 07:33:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Chi"
+var height = "65"
+var hobby = "Weaving" + " " + "and" + " " + "Dancing"
+var socmed= "whatchidid"
+
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1)
+
+console.log("Hobby:")
+console.log(hobby);
+
+console.log("Social Media Handle:")
+console.log(socmed);
