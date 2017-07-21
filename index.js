eed98700de20683eccb930de13fc179664373a4c
wobblewedge <andobrown71@gmail.com>
2017-07-21 15:46:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Andrew"
+var height = 70
 
-console.log("Name:")
-console.log(name)
+console.log("Name: " + name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height: " + (height + 1))
