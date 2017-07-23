8c8acc30585f4d0aec309eeedbcad9860ec463d2
Michael Jackson <mj94@mj94.net>
2017-07-23 00:50:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// write your code below!
+var name = "Michael"
+var height = 61
 
-var name = "Joe"
-var height = "74"
+console.log("Name: " + name)
 
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height)
+console.log("Height: " + (height + 1))
