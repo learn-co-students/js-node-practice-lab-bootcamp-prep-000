c9a66ea453d163fc59a9c2bf919cfb9c6d424b6c
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-08-13 18:50:40 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 var name = "Joe"
 var height = "74"
+var name = "Janet"
 
-console.log("Name:")
+console.log("Joe:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1 )
