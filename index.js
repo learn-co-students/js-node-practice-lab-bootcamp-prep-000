15e0edf5d17e4ec235ca5c94bed82ab84e90f2f6
Karen Truss <truss.karen@gmail.com>
2017-08-21 01:46:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// write your code below!
+{// write your code below!
 
 var name = "Karen"
-var height = 74
+var height = 61
 
 console.log("Name:")
 console.log(name)
-
 console.log("Height:")
-console.log(height+1)
+console.log(height+3)
+}
