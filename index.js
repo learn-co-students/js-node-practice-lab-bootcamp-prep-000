911fab20474437ac5d0a8c0efd92f731bae2d876
Benjamin Silfen <bfsilfen@gmail.com>
2017-08-27 13:42:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Ben"
-var height = "72"
+var height = 72
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
