bb23c90b7150d138dbf6c22942ca4473b87803cd
Ryan Mascolo <rjmascolo@gmail.com>
2017-08-28 00:34:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Ryan"
+var height = "84"
 
-console.log("Name:")
+console.log("Name:" + name)
 console.log(name)
 
-console.log("Height:")
+console.log("Height:" + height)
 console.log(height)
