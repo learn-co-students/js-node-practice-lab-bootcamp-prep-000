fdc27f1af608830dde9b530b2da55f207081bb73
shravya r <yagnakashyap@gmail.com>
2017-09-17 01:58:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Shravya"
-var height = 65
+var height = 85
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height+5))
+console.log(parseInt(height+3))
