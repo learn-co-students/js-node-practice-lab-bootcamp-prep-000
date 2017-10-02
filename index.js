51d94b6364bd5edc32377cb3cb8f23c1a48df5dc
Peter Ciavarella <peterciavarella@gmail.com>
2017-10-02 20:58:48 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 var name = "Janet"
 var height = "73"
-console.log(parseInt(height) + 1 )
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1 )
