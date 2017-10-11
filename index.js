c300fb1e50bc9257802a041ace6103ca80e23870
Laura Slor <slorlaura@gmail.com>
2017-10-11 02:16:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Laura"
-var height = "53"
+var height = 53
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log((height) + 1)
