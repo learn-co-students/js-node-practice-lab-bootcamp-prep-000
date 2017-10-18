a20bba28eb1882203810cbf9acf234bab2a998d5
Katie Stout <katiels9@me.com>
2017-10-18 23:53:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 var name = "Katie"
-var height = "54"
+var height = 54
 
 console.log("Name:")
 console.log(name)
@@ -9,4 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height)
 
-console.log(parseInt(height) + 1 )
+console.log(height + 1 )
