7eb373a6f5837fc162b76e841a8a61b95effa1ea
Helen Liutongco <bellpickle@gmail.com>
2017-10-20 03:59:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Helen"
+var height = 62
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
