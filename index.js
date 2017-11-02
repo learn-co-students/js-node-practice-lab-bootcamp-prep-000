5a755a3e93f3830026ed7fa76aca247d03752304
renacka burgess <renacka@aol.com>
2017-11-02 01:21:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Renacka"
-var height = 74
+var height = "54"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height + 1)
+console.log(parseInt(height) + 1 )
