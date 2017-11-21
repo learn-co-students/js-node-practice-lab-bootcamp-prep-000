c028694efc3fbb495c6b78a49359dd487f79cc3a
Benjamin Johnson <odiowab@gmail.com>
2017-11-21 23:46:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 var name = "Joe"
 var height = "74"
 
+name = "Benjamin"
+
+
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1)
