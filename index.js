250440af7894cc236d27d5277bf21520a73a1618
sameerahrshaik@gmail.com Shaik <sameerahrshaik@gmail.com>
2017-11-26 05:27:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Janet"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
