f45acffaeeb3779b67036d3e16621c9a7c552720
Dawn Ann Beattie <heartandhandstraining@yahoo.com>
2017-12-18 04:06:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// write your code below!
+// write your code below! Use "Run" instead of typing "nodejs index.js" to run the code.
 
-var name = "Joe"
-var height = "74"
+var name = "Dawn"
+var height = "70"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+console.log(parseInt(height) + 1)
