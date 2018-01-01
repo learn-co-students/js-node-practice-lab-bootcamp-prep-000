6a17ae56f7ad7a1bf85dc9443110645d415b37f4
William Robertson <william02robertson@gmail.com>
2018-01-01 10:24:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// write your code below!
-
-var name = "Joe"
-var height = "74"
-
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height)
+// Define Variables
+var name = "William";
+var height = 65;
+// Call Variables
+console.log(`Name: ${name}`);
+console.log(`Height: ${height + 1}`);
