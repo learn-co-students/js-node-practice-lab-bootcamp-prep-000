2833839f5850c0a9fb7713255bf10ffeb52f8648
Iho Somnam <ihosomnam4741@gmail.com>
2018-01-11 11:16:41 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Joe"
 var height = "74"
 
-console.log("Name:")
+console.log("Name: " + name)
 console.log(name)
 
-console.log("Height:")
+console.log("Height: " + height)
 console.log(height)
