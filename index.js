5d4d432ddf95d448d4604a2f837a3905362a0168
Mohammad Liaquat Ali <ayon88@hotmail.com>
2018-01-12 22:25:49 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var name = "Joe"
 var height = "74"
+name = "Mohammad"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+console.log(parseInt(height) + 1);
