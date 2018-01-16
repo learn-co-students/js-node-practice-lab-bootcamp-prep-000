0a22b3e856d9439ea6671e275be7a25dfe3227ff
Candece Berry <candece.berry@gmail.com>
2018-01-16 05:31:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Candece"
 var height = "74"
 
-console.log("Name:")
+console.log("Name:Candece")
 console.log(name)
 
-console.log("Height:")
+console.log("Height:74")
 console.log(height)
