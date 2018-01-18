bc3ff4dee2780164c1b95fa591870364ba7525c3
Douglas Craig <dcraig112@gmail.com>
2018-01-18 18:25:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// write your code below!
+var name = "doug"
+var height = 72
 
-var name = "Doug"
-var height = 74
-
-console.log("Name:")
 console.log(name)
-
-console.log("Height:")
-console.log(height +1)
+console.log(height +2)
