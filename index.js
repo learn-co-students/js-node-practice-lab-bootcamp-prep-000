4442c0359655b6c5a98ebe523575fe68d13f6091
Jared Kelsay <jaredkelsay@gmail.com>
2018-01-24 17:31:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Jared"
-var height = "69"
+var height = 69
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)) + 1
+console.log(height + 1)
