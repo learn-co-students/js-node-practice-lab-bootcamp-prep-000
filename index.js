dd19c0432df4040234683138612a6e57bf5aa35f
cherylngql@gmail.com <cherylngql@gmail.com>
2018-02-11 23:05:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Cheryl"
+var height = 61
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
