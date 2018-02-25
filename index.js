a32e1d0bf064f7b41213c97e3b76d232d1785416
Henry Chan <henry.chan64@gmail.com>
2018-02-25 23:15:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Henry"
+var height = "67"
+var height2 = 67
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
+console.log(height2)
+console.log(height2+1)
