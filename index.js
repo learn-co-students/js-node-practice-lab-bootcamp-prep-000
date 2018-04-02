3ac5ff7ec9c450b1a8140a902393fc163c8eb8a0
Luke Alpers <alpersluke@gmail.com>
2018-04-02 19:13:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Luke"
+var height = 77
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+// stop using a string to store a number if you don't need to!!
