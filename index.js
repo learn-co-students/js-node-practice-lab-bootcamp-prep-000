515c5948aad923bbb64b934b5c38821eb2c97029
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-08 04:47:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Robin Dao";
+var height = "160";
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(parseInt(height)+ 1);
