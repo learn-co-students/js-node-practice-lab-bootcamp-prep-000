d8737e5838bbf02796498ba45ba9667ffacd999e
Anallely Curiel <anallely.curiel@gmail.com>
2018-05-07 16:31:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = 'Anallely'
+var height = 30
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+//console.log(parseInt(height)+1)
+console.log(height+1)
