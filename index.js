e3cf72a900621e8bdec9ec301a9c4a9506a8c132
arianna carter <carterari43@gmail.com>
2018-05-28 05:37:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Ari"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+
+console.log(height+1)
