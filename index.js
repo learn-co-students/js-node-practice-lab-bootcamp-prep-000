80129740ad9c51f97b093ef7113ff7ccbb5d5222
Andrew Sommer <sommer.drew@gmail.com>
2018-05-31 13:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Drew"
+var height = "67"
+
+height = parseInt(height)
+
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+5)
