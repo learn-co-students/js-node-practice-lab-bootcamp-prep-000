a570e718737cbddff28725a15bc8d86c1df3ac97
Rasheed Anbali Adebayo <rasheed.bayo@gmail.com>
2018-06-02 16:11:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log ("Name")
+console.log ("Adebayo")
+var name = ("Bisola")
+console.log("54")
+console.log(height+1)
+console.log(parseInt(height)+1
+
+console.log ("Olaitan")
+console.log("68")
+console.log(name)
+
+var name = "Joe"
+var height = "74"
+
+console.log("Name:")
+console.log(name)
+
+console.log("Height
+console.log(height +1)
+
