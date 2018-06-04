fb81716103cd81b6f81444f186e662ad6ea47a83
Ki Ki  Bush <donyabush@gmail.com>
2018-06-04 23:09:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Donya"
-var height = "70"
+var height = 70
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+5)
+console.log(height + 4)
