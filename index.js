373d7c073893b69512510041db80765a5cf4777e
sofia.i.rahimi@gmail.com Isabelle <sofia.i.rahimi@gmail.com>
2018-06-07 23:21:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Sofia Isabelle Rahimi";
-var height = "5ft 3in";
+var height = "129.54";
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height + 1)
