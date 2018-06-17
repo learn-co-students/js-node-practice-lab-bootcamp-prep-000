6db58992648f55d1552b8d322f625adb1169b211
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-17 20:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Dickbutt"
+var height = 5*12+10
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
