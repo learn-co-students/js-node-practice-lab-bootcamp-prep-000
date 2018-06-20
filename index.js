cdffd64db2d663bcabead7fc01e70cf17df3f810
brittany collier <prettyawesomeswagg@gmail.com>
2018-06-20 01:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = 'baby hxze';
+var name = "baby hxze";
 var height = "66"
 
-console.log(baby hxze:")
-console.log(baby hxze)
-
+console.log ("baby hxze")
+console.log("baby hxze")
+""
 console.log("66:")
 console.log(66+1)
