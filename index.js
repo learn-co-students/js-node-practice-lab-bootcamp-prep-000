3701babf5d958c3354b689476c1a316e91edf56d
Min Kim <minmkim@gmail.com>
2018-07-03 21:34:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Min"
-var height = "73"
+var height = 73
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) - 1)
+console.log(height - 1)
