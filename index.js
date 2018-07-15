53555b126ee2e8c80ad04531557a13cfeba12b1a
Madeline Celis <madelinebcelis@gmail.com>
2018-07-15 02:47:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Janet"
+var height = 60
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height + 1);
