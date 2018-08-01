974f3436975dd7d36402f7cd050fd5ad900dfce7
Malik Glossop <glossopm@users.noreply.github.com>
2018-08-01 00:24:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Malik"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+114)
+console.log(height+1)
