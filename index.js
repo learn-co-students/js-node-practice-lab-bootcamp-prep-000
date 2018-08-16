9a4eb499b8b8570ce7aeb840b25a5e728c610d56
miles <milesdsturgeon@gmail.com>
2018-08-16 20:33:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "David"
+var height = 70 //removed quotes to make number not string
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+//console.log(parseInt(height) + 1)
