6e57a6d8eb737dba0be3b5e738c7a01516aa6798
Aman Kaushik <aman.kaushik@engineer.com>
2018-08-21 18:28:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Joy"
 var height = "74"
 
-console.log("Name:")
+console.log("Name:Janet")
 console.log(name)
 
-console.log("Height:")
+console.log("Height:75")
 console.log(height)
