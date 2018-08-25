92fe89469962041f191be8c00748ce487903ae42
Adrian Head <adrianhead1@gmail.com>
2018-08-25 21:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Adrian"
+var height = 74 + 1
 
-console.log("Name:")
+console.log("Name")
 console.log(name)
 
 console.log("Height:")
