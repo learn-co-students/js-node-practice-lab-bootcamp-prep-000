3cb4c8c73c2c15dc2858fe24527f9452fb13eda9
dlrsheets@gmail.com <dlrsheets@gmail.com>
2018-08-27 18:00:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Bluepaw17"
+var height = 118
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height +1)
