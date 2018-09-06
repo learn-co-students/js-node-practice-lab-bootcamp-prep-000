4f6a2344ebab97640650d2395efe3a5dcde9f694
Gabriel Frink <frinkdotdesign@gmail.com>
2018-09-06 12:56:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Janet"
+var height = 74
 
-console.log("Name:")
+console.log(height + 1)
 console.log(name)
 
 console.log("Height:")
