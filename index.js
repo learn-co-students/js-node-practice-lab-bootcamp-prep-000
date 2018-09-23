65007f1b356fae150949d6c9f2dbc9aafcd6ae4e
Miyuki Abe <miyuki0809@gmail.com>
2018-09-23 21:15:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // write your code below!
 
 var name = "Miyuki"
-var height = 5
+// var height = 5
+
+
+
+
+//let name = "Joe";
+let height = 74;
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height+1)
+
+
+
+
+
+
