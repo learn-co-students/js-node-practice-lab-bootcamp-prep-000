a3f74de2c736fea0b95b37413c8bf6988ea26385
Jonathan Koss <jonathan_koss@yahoo.com>
2018-09-26 01:03:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Jonathan"
-var height = 73
+var name = "Jonathan";
+var height = 73;
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height+1)
+console.log("Height:");
+console.log(height +1);
