5c7721673b51060108e179a961e571eb125d5989
Brooke Gill <brooke.gill25@gmail.com>
2018-10-17 01:03:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Brooke"
 var height = 62
 
-console.log("Name: "+name)
-// console.log(name)
+console.log("Name:")
+console.log(name)
 
-console.log("Height: "+(height+1))
-// console.log(height + 1)
+console.log("Height:")
+console.log(height + 1)
