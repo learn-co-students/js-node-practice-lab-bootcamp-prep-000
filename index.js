59b815f0bcadec1c36bb95a391de1e0c83ee522e
Travis Martin <travisemerymartin@gmail.com>
2018-10-26 18:59:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // write your code below!
 
 var name = "Travis"
-var height = "75"
+var height = 75
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+console.log("Height w/ Growth:")
+console.log(height+1)
