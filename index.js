5ce7332df7d254b3077e4687269787930365331c
Colten Rhoades <coltentr@rocketmail.com>
2019-01-12 04:32:44 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Joe";
 var height = "74";
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height);
