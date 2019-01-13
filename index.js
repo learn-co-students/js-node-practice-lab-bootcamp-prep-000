8211652ef9aa05e9c3efddc229584c6509135bb6
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-13 14:36:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Garrett"
 var height = "74"
+var height1 = 74
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height (parseInt():")
+console.log(parseInt(height)-2) //parseInt()
+console.log("Height (int):")
+console.log(height1 - 2) //int
