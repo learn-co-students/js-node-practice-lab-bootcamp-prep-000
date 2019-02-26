f382a4580c4312b5fea4cf6912c80b9f7ccf99d1
Nicholas Stano <nicholasstano@gmail.com>
2019-02-26 00:44:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// write your code below!
+var name = "Janet"
+var height = '74'
 
-var name = 'Nick'
-var height = "74"
-
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(parseInt(height)+1)
-
+console.log("Name:");
+console.log(name);
+ 
+console.log('Height:');
+console.log(parseInt(height) + 1);
