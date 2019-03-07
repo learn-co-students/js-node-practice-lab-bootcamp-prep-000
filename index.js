c9d0af7019d58b6947c68c4b57f1ec068bf98cff
Taylor Watson <taylortwatson@gmail.com>
2019-03-07 21:26:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Taylor"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log(height-5)
