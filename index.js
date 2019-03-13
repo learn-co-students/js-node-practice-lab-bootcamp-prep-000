616800953185d992a3acccd168063f6e1bb12bbb
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-03-13 15:14:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Ioan"
+var height = "170" //Because I'm familar with metrtic not imperial :)
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 2 ) //I guess I might be taller...
