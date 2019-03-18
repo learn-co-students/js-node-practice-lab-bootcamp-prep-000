f72e07aed786c0f95ca35798b64c63bfb131e8de
Shukri Abdi <sabdi21@hotmail.com>
2019-03-18 20:23:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Shukri"
 var height = "74"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height + 1)
+// console.log(parseInt(height) + 1 )
