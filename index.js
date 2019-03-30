36c5f3e1445399a5010194a6b695c03d707a1ca2
Zach Alden <zach@zach-alden.com>
2019-03-30 22:51:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Turbo"
-var height = 69
+var name = 'Turbo'
+var height = '666'
 
-console.log("Name:")
+console.log('Name:')
 console.log(name)
 
-console.log("Height:")
-console.log(height + 1)
+console.log('Height:')
+console.log(height)
