2c3fe5bfa75c33535c7d0a4eba90c77ba49cdd2c
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-17 20:38:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Alex"
-var height = 74
+var name = "Alex";
+var height = 74;
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log("Height:");
+console.log(parseInt(height) + 1);
