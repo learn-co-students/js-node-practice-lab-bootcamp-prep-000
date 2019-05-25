638bec33476833575e83932bcbe21eddc6fdb636
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-25 19:29:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Big Z"
 var height = "74"
 
-console.log("Name:")
-console.log(name)
+console.log("Name: "+name)
+//console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height: "+parseInt(height-1))
+//console.log(height)
