84041d46be687bb4986f6ba46d2ec3543396e3c2
srikar kaliki <kalikisrikar@gmail.com>
2019-06-05 01:49:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "srikar"
-var height = "57"
+var height = 57
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
