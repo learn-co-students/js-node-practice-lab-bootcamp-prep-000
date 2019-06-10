2e1182cb77671fb23d87cb78cda524bfe2c2aa1c
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-10 13:41:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Christopher"
+var height = 80
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
+
+
+
