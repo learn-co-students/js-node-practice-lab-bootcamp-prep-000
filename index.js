c1466f2fc045f3b53eec134fbb352f16ea5ac97f
Matthew Heavner <mrh9ud@virginia.edu>
2019-06-26 20:49:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Matt"
+var height = 70
 
-console.log("Name:")
-console.log(name)
+console.log("Name: " + (name))
 
-console.log("Height:")
-console.log(height)
+console.log("Height: " + parseInt(height + 1))
