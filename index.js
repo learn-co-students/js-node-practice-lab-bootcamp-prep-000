d8cba0be2a696bb9e03530c3754c5bb435c6377f
Barbara Muesing <muesingb@gmail.com>
2019-07-18 01:25:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Barbara"
-var height = 68;
+var name = "Barbara";
+var height = 69;
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height+1);
