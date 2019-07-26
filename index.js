9ebc954d079560e8dbbd5788ebfb1f65356240c8
GLEISON OLIVEIRA COSTA <gleisoncosta705@gmail.com>
2019-07-26 01:08:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Gleison"
 var height = "74"
 
-console.log("Name:")
-console.log(name)
+console.log(`Name: ${name}`)
 
-console.log("Height:")
-console.log(height)
+height = parseInt(height)
+console.log(`Height: ${height}`)
+
+module.exports = { name, height
+}
