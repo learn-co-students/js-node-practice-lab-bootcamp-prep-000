d8f350c2421f9e0cf6be36075501c40c1661a62e
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-02 20:31:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // write your code below!
 
-var name = "Joe"
-var height = "74"
+var name = "Kim"
+var height = "63"
+
+height = parseInt(height)
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height + 1)
