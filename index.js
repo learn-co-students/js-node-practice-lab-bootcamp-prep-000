17cfa81edaf59068464f5120585249841aa0af5b
Diori Linares <diori90@gmail.com>
2019-09-03 02:32:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
-var name = name
-var height = "74"
+var name = 'Diori'
+var height = 68
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 //console.log(height)
-console.log(parseInt(height) + 1)
+console.log(height + 1)
