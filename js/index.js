8fdb0b07ff88fae127ba9b35a47dc1737c8e0aac
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-27 21:48:24 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Nhon"
-var height = "74"
+var height = 74;
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 )
+console.log(height + 1 )
 
