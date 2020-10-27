6547241d0d06c4aefde1d03a4e3562c0f0772890
ultimate-tuple-4675 <rauggie2@hotmail.com>
2020-10-27 02:47:46 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-var name = "Janet"
 var height = 74;
 
-console.log(Joe)
+console.log(Joe);
 console.log(name);
 
-console.log('Height:')
-console.log(height+10)
+console.log('Height:');
+console.log(height+10);
